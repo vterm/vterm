883a820a8da5396ba71bb41255bca793906f6261
Luca <LucaT1@users.noreply.github.com>
2017-06-16 20:24:00 +0200

    :recycle: Improved loader and moved notify lib

--- src/utils/loader.js
+++ src/utils/loader.js
@@ -1,7 +1,13 @@
-import Module from 'module'
+import Module      from 'module'
 import * as Preact from 'preact'
-import Store from '../store'
-import Notify from '../notify'
+import * as mobx   from 'mobx'
+import mobxPreact  from 'mobx-preact'
+
+import * as Notify from '../actions/notify'
+import * as Tabs   from '../actions/tabs'
+import * as Window from '../actions/window'
+
+import Store       from '../store'
 import * as Colors from '../styles/colors'
 
 const __load = Module._load
@@ -11,12 +17,26 @@ Module._load = function (path) {
   switch (path) {
     case 'preact':
       return Preact
+
+    case 'mobx':
+      return mobx
+
+    case 'mobx-preact':
+      return mobxPreact
+
     case 'yat/store':
       return Store
-    case 'yat/notify':
-        return Notify
+
+    case 'yat/actions':
+      return {
+        notify: Notify,
+        tabs: Tabs,
+        window: Window
+      }
+
     case 'yat/colors':
-        return Colors
+      return Colors
+
     default:
       return __load.apply(this, arguments)
   }
