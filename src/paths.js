8be082d8868fe21d9ecac8d352abf66a48cfbdfa
Luca <LucaT1@users.noreply.github.com>
2017-06-21 17:29:59 +0200

    :pen: Alignments

--- src/paths.js
+++ src/paths.js
@@ -1,8 +1,7 @@
-import { join } from 'path'
+import { join }    from 'path'
 import { homedir } from 'os'
 
-export { join } from 'path'
-export const base = join( homedir(), '.yat' )
-export const config = join( base, 'config.js' )
-export const babelrc = join( base, '.babelrc' )
-export const plugins = join( base, 'plugins' )
+export const base    = join( homedir(), '.yat'      )
+export const config  = join( base,      'config.js' )
+export const babelrc = join( base,      '.babelrc'  )
+export const plugins = join( base,      'plugins'   )
