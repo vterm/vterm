4c7fb968bf86720a0df961b41ccce9cdf993ddb2
Luca <LucaT1@users.noreply.github.com>
2017-06-21 19:28:42 +0200

    :wrench: Use devtools only in development mode

--- main.js
+++ main.js
@@ -2,11 +2,19 @@ const {app, BrowserWindow, ipcMain} = require('electron')
 
 let win
 app.on('ready', () => {
-  win = new BrowserWindow({width:800, height: 600, transparent: true, frame: false})
+  win = new BrowserWindow({
+    width:800,
+    height: 600,
+    transparent: true,
+    frame: false
+  })
+
   win.hide()
   win.setMenu(null)
   win.loadURL(`file://${__dirname}/dist/index.html`)
-  win.webContents.openDevTools()
+
+  // Show devtools only if you're in development mode
+  if(process.env.NODE_ENV == 'development') win.webContents.openDevTools()
 })
 
 ipcMain.on('ready', () => win.show())
