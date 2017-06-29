444d76b2a9b897cddfc524dde3a1f8210f551e85
Luca <LucaT1@users.noreply.github.com>
2017-06-29 23:39:22 +0200

    :diamonds: Stdout/Stderr builds

--- scripts/publish-ci.js
+++ scripts/publish-ci.js
@@ -2,6 +2,10 @@ const { platform } = require('os')
 const { spawn }    = require('child_process')
 const { join }     = require('path')
 
-const yarn = join(__dirname, '..', 'node_modules', '.bin', 'yarnpkg')
+const yarn = join(__dirname, '..', 'node_modules', '.bin', 'yarn')
 
-spawn(yarn, ['run', 'build:' + platform() + ':p'])
+const process = spawn(yarn, ['run', 'build:' + platform() + ':p'])
+process.stdout.on('data', console.log)
+process.stderr.on('data', console.warn)
+
+process.on('close',(code) =>  console.log(`Exited with ${code}`))
