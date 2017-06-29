d416ad106e126ed121753521ffe6291caf514929
Luca <LucaT1@users.noreply.github.com>
2017-06-29 19:53:32 +0200

    :hammer: Fixed buggy font

--- src/defaults/font.js
+++ src/defaults/font.js
@@ -1,1 +1,1 @@
-export default `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif`
+export default `-apple-system, BlinkMacSystemFont, "Operator Mono", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", courier-new, courier, monospace`
