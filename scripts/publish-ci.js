ca62e2b385c5cc418bc3f9fdea280d9ea0d1cc64
Luca <LucaT1@users.noreply.github.com>
2017-06-29 23:41:46 +0200

    :abc: Stringify stdouts

--- scripts/publish-ci.js
+++ scripts/publish-ci.js
@@ -5,7 +5,7 @@ const { join }     = require('path')
 const yarn = join(__dirname, '..', 'node_modules', '.bin', 'yarn')
 
 const process = spawn(yarn, ['run', 'build:' + platform() + ':p'])
-process.stdout.on('data', console.log)
-process.stderr.on('data', console.warn)
+process.stdout.on('data', (data) => console.log(data.toString()))
+process.stderr.on('data', (err) => console.warn(err.toString()))
 
 process.on('close',(code) =>  console.log(`Exited with ${code}`))
