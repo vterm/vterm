8f91657df976cae8a13cad7962c20910dc7d3eb1
Luca <LucaT1@users.noreply.github.com>
2017-06-30 07:48:52 +0200

    :up: Use default yarn cli

--- scripts/publish-ci.js
+++ scripts/publish-ci.js
@@ -2,9 +2,7 @@ const { platform } = require('os')
 const { spawn }    = require('child_process')
 const { join }     = require('path')
 
-const yarn = join(__dirname, '..', 'node_modules', '.bin', 'yarn')
-
-const process = spawn(yarn, ['run', 'build:' + platform() + ':p'])
+const process = spawn('yarn', ['run', 'build:' + platform() + ':p'])
 process.stdout.on('data', (data) => console.log(data.toString()))
 process.stderr.on('data', (err) => console.warn(err.toString()))
 
