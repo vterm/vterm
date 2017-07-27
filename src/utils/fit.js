a8c7fcf6598848951a83033216c58790bce05d5d
Luca <LucaT1@users.noreply.github.com>
2017-07-28 01:20:24 +0200

    :fire: Fix terminal calculation with padding

--- src/utils/fit.js
+++ src/utils/fit.js
@@ -4,8 +4,8 @@ import { PADDING } from '../defaults/variables'
 export default (ele, tester) => {
   const padding = Store.config.padding || PADDING
   const style   = window.getComputedStyle(ele)
-  const _height = parseInt(style.getPropertyValue('height')) - padding
-  const _width  = Math.max(0, parseInt(style.getPropertyValue('width')) - padding)
+  const _height = parseInt(style.getPropertyValue('height')) - padding * 2
+  const _width  = Math.max(0, parseInt(style.getPropertyValue('width')) - padding * 2)
 
   const { width }      = tester.getBoundingClientRect()
   tester.style.display = ''
