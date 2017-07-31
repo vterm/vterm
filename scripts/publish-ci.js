35f34f7471456f61df5e5719f015141574a0b9d4
Luca <LucaT1@users.noreply.github.com>
2017-08-01 01:09:12 +0200

    :green_heart: Use bash with TravisCI

--- scripts/publish-ci.js
+++
@@ -1,9 +0,0 @@
-const { platform } = require('os')
-const { spawn }    = require('child_process')
-const { join }     = require('path')
-
-const process = spawn('yarn', ['run', 'build:' + platform() + ':p'])
-process.stdout.on('data', (data) => console.log(data.toString()))
-process.stderr.on('data', (err) => console.warn(err.toString()))
-
-process.on('close',(code) =>  console.log(`Exited with ${code}`))
