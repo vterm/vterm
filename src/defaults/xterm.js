47ed900aee99d7ea4b370bcad713038035f0ce45
Luca <LucaT1@users.noreply.github.com>
2017-08-04 01:19:38 +0200

    :art: Update xterm's css :tada:

--- src/defaults/xterm.js
+++ src/defaults/xterm.js
@@ -1,17 +1,61 @@
-import Store             from '../store'
-import { grey }          from '../defaults/colors'
-import { BORDER_COLOR }  from './variables'
+import Store    from '../store'
+import { grey } from '../defaults/colors'
 
-const { cursorColor, primaryColor } = Store.config
+// Import defaults
+import {
+  BORDER_COLOR,
+  FOREGROUND
+}  from './variables'
+
+// Extract values from the Store
+const {
+  cursorColor,
+  foreground
+} = Store.config
 
 export default `
+/**
+ * xterm.js: xterm, in the browser
+ * Copyright (c) 2014-2016, SourceLair Private Company (www.sourcelair.com (MIT License)
+ * Copyright (c) 2012-2013, Christopher Jeffrey (MIT License)
+ * https://github.com/chjj/term.js
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * Originally forked from (with the author's permission):
+ *   Fabrice Bellard's javascript vt100 for jslinux:
+ *   http://bellard.org/jslinux/
+ *   Copyright (c) 2011 Fabrice Bellard
+ *   The original design remains. The terminal itself
+ *   has been extended to include xterm CSI codes, among
+ *   other features.
+ */
+
+/*
+ *  Default style for xterm.js
+ */
+
 .terminal {
-  background-color: transparent;
+  color: ${ foreground || FOREGROUND };
   font-feature-settings: "liga" 0;
   position: relative;
   user-select: none;
-  -ms-user-select: none;
-  -webkit-user-select: none;
 }
 
 .terminal.focus,
@@ -57,52 +101,40 @@ export default `
   text-decoration: none;
 }
 
-.terminal.focus:not(.xterm-cursor-style-underline):not(.xterm-cursor-style-bar) .terminal-cursor {
-  background-color: ${cursorColor || primaryColor || BORDER_COLOR};
-  color: #000;
+.terminal .terminal-cursor {
+  color: ${ cursorColor || BORDER_COLOR };
+  position: relative;
 }
 
 .terminal:not(.focus) .terminal-cursor {
-  outline: 1px solid ${cursorColor || primaryColor || BORDER_COLOR};
+  outline: 1px solid ${ cursorColor || BORDER_COLOR };
   outline-offset: -1px;
-  background-color: transparent;
 }
 
-.terminal:not(.xterm-cursor-style-underline):not(.xterm-cursor-style-bar).focus.xterm-cursor-blink-on .terminal-cursor {
-  background-color: transparent;
-  color: inherit;
+.terminal.xterm-cursor-style-block.focus:not(.xterm-cursor-blink-on) .terminal-cursor {
+  background-color: ${ cursorColor || BORDER_COLOR };
+  color: ${ foreground || FOREGROUND };
 }
 
-.terminal.xterm-cursor-style-bar .terminal-cursor,
-.terminal.xterm-cursor-style-underline .terminal-cursor {
-  position: relative;
-}
-.terminal.xterm-cursor-style-bar .terminal-cursor::before,
-.terminal.xterm-cursor-style-underline .terminal-cursor::before {
-  content: "";
-  display: block;
+.terminal.focus.xterm-cursor-style-bar:not(.xterm-cursor-blink-on) .terminal-cursor::before,
+.terminal.focus.xterm-cursor-style-underline:not(.xterm-cursor-blink-on) .terminal-cursor::before {
+  content: '';
   position: absolute;
-  background-color: ${cursorColor || primaryColor || BORDER_COLOR};
+  background-color: ${ cursorColor || BORDER_COLOR };
 }
-.terminal.focus.xterm-cursor-style-bar .terminal-cursor::before {
+
+.terminal.focus.xterm-cursor-style-bar:not(.xterm-cursor-blink-on) .terminal-cursor::before {
   top: 0;
-  bottom: 0;
   left: 0;
-  width: 3px;
+  bottom: 0;
+  width: 2.5px;
 }
-.terminal.xterm-cursor-style-underline .terminal-cursor::before {
+
+.terminal.focus.xterm-cursor-style-underline:not(.xterm-cursor-blink-on) .terminal-cursor::before {
   bottom: 0;
   left: 0;
   right: 0;
   height: 1px;
-}
-.terminal.xterm-cursor-style-bar.focus.xterm-cursor-blink.xterm-cursor-blink-on .terminal-cursor::before,
-.terminal.xterm-cursor-style-underline.focus.xterm-cursor-blink.xterm-cursor-blink-on .terminal-cursor::before {
-  background-color: transparent;
-}
-.terminal.xterm-cursor-style-bar.focus.xterm-cursor-blink .terminal-cursor::before,
-.terminal.xterm-cursor-style-underline.focus.xterm-cursor-blink .terminal-cursor::before {
-  background-color: ${cursorColor || primaryColor || BORDER_COLOR};
 }
 
 .terminal .composition-view {
@@ -120,7 +152,7 @@ export default `
 .terminal .xterm-viewport {
   /* On OS X this is required in order for the scroll bar to appear fully opaque */
   background-color: transparent;
-  overflow-y: auto;
+  overflow-y: scroll;
 }
 
 .terminal .xterm-wide-char,
@@ -166,9 +198,8 @@ export default `
 
 .terminal .xterm-selection div {
   position: absolute;
-  background-color: ${cursorColor || primaryColor || BORDER_COLOR};;
+  background-color: ${ cursorColor || BORDER_COLOR };
 }
-
 
 /*
  *  Determine default colors for xterm.js
@@ -183,6 +214,10 @@ export default `
 
 .terminal .xterm-blink {
   text-decoration: blink;
+}
+
+.terminal .xterm-blink.xterm-underline {
+  text-decoration: blink underline;
 }
 
 .terminal .xterm-hidden {
